353f0b4e996ca9537d697b2d259b0de0dce645ef
gkobilansky <genek149@gmail.com>
2015-01-30 12:22:59 -0500

    Added Slingshot & Created A Custom Post Field

--- packages/telescope-theme-tdvids/lib/client/tdvids.js
+++ packages/telescope-theme-tdvids/lib/client/tdvids.js
@@ -4,3 +4,20 @@ heroModules.push({
   template: 'tagline',
   order: 0
 });
+
+
+// Add Upload Post Field
+
+var thumbnailProperty = {
+  propertyName: 'thumbnailUrl2',
+  propertySchema: {
+    type: String,
+    optional: true,
+    label: 'thumbnail2'
+
+  }
+}
+
+addToPostSchema.push(thumbnailProperty);
+
+addToPostSchema.unshift(thumbnailProperty);
